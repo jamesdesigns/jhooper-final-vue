07e92d26a6288acd4b5adc59d63315b24e54aace
James Hooper <jameshooperdesigns@yahoo.com>
2018-10-15 15:25:24 -0600

    Added Objects/JavaScript files and added this into the About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ created }}This is an about page</h1>
   </div>
 </template>
+<script>
+import { films } from "../assets/films";
+export default {
+  created: function() {
+    console.log(films); // Text goes here
+  }
+}
+</script>
+<style>
+
+</style>
