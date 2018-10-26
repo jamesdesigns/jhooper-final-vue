1e9c3c2b823938bbc26762db6fde0e6c3a9af7cd
James Hooper <jameshooperdesigns@yahoo.com>
2018-10-26 15:34:30 -0600

    Added a Form Login that includes a Login Button

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,6 @@
 <template>
-    <v-container grid-list-md>
+   <!-- <v-container grid-list-md>
         <v-layout justify-center>
-           <!-- <v-flex xs6>  -->
                <v-flex xs6>
                 <v-card>
                 <v-form>
@@ -24,9 +23,42 @@
                 </div>
         </v-layout>
     </v-container>
+    -->
+<v-layout row justify-center>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-btn slot="activator" color="primary" dark>Login</v-btn>
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Login</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              
+              <v-flex xs12>
+                <v-text-field label="Email*" required></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Password*" type="password" required></v-text-field>
+              </v-flex>
+              
+            </v-layout>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="dialog = false">Submit</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-layout>
+
+
 </template>
 <script>
-export default {
+/* export default {
     data () {
         return {
             show1: false,
@@ -35,6 +67,13 @@ export default {
         }
     }
 }
+*/
+
+ export default {
+    data: () => ({
+      dialog: false
+    })
+  }
 </script>
 <style scoped>
 </style>
