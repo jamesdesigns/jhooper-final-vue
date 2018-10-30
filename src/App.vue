0aca2f5dcb28f5833fd351f834c2a5e62d24515b
James Hooper <jameshooperdesigns@yahoo.com>
2018-10-29 22:13:54 -0600

    Remove the footer from the App

--- src/App.vue
+++ src/App.vue
@@ -8,55 +8,12 @@
       <router-link to="/login">Login</router-link>
     </div>
     <router-view/>
-
-
-
-<v-footer
-    dark
-    height="auto"
-  >
-    <v-card
-      class="flex"
-      flat
-      tile
-    >
-      <v-card-title class="grey lighten-1">
-       
-
-        <v-spacer></v-spacer>
-
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-3"
-          dark
-          icon
-        >
-          <v-icon size="24px">{{ icon }}</v-icon>
-        </v-btn>
-      </v-card-title>
-
-      <v-card-actions class="black darken-3 justify-center">
-        &copy;2018 — <strong>Stock Charts</strong>
-      </v-card-actions>
-    </v-card>
-  </v-footer>
-
-
-
   </div>
 </template>
 
 <script>
  export default {
-    data: () => ({
-       icons: [
-        'fab fa-facebook',
-        'fab fa-twitter',
-        'fab fa-google-plus',
-        'fab fa-linkedin',
-        'fab fa-instagram'
-        ],  
+    data: () => ({ 
       links: [
         'Home',
         'About Us',
