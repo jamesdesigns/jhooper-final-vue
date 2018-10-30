01e6732391f3882ca2e8792a2463774ab237bc34
James Hooper <jameshooperdesigns@yahoo.com>
2018-10-29 23:04:36 -0600

    Add timeline

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Stock Charts"/>
+    <HelloWorld msg="Create a To Do List"/>
   </div>
 </template>
 
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,262 +1,133 @@
 <template>
   <div>
-   
-
- <v-data-table
-    :headers="headers"
-    :items="desserts"
-    class="elevation-1"
+ <v-card
+    class="mx-auto"
+    max-width="1200"
   >
-    <template slot="headerCell" slot-scope="props">
-      <v-tooltip bottom>
-        <span slot="activator">
-          {{ props.header.text }}
-        </span>
-        <span>
-          {{ props.header.text }}
-        </span>
-      </v-tooltip>
-    </template>
-    <template slot="items" slot-scope="props">
-      <td>{{ props.item.name }}</td>
-      <td class="text-xs-right">{{ props.item.price }}</td>
-      <td class="text-xs-right">{{ props.item.volume }}</td>
-      <td class="text-xs-right">{{ props.item.carbs }}</td>
-      <td class="text-xs-right">{{ props.item.protein }}</td>
-      <td class="text-xs-right">{{ props.item.iron }}</td>
-    </template>
-  </v-data-table>
+    <v-card
+      dark
+      flat
+    >
+      <v-btn
+        absolute
+        bottom
+        color="pink"
+        right
+        fab
+      >
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+      <v-card-title class="pa-2 blue">
+        <v-btn icon>
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+        <h3 class="title font-weight-light text-xs-center grow">Timeline</h3>
+        <v-avatar>
+          <v-img src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"></v-img>
+        </v-avatar>
+      </v-card-title>
+      <v-img
+        src="https://cdn.vuetifyjs.com/images/cards/forest.jpg"
+        gradient="to top, rgba(0,0,0,.44), rgba(0,0,0,.44)"
+      >
+        <v-container fill-height>
+          <v-layout align-center>
+            <strong class="display-4 font-weight-regular mr-4">8</strong>
+            <v-layout column justify-end>
+              <div class="headline font-weight-light">Monday</div>
+              <div class="text-uppercase font-weight-light">February 2015</div>
+            </v-layout>
+          </v-layout>
+        </v-container>
+      </v-img>
+    </v-card>
+    <v-card-text class="py-0">
+      <v-timeline
+        align-top
+        dense
+      >
+        <v-timeline-item
+          color="pink"
+          small
+        >
+          <v-layout pt-3>
+            <v-flex xs3>
+              <strong>5pm</strong>
+            </v-flex>
+            <v-flex>
+              <strong>New Icon</strong>
+              <div class="caption">Mobile App</div>
+            </v-flex>
+          </v-layout>
+        </v-timeline-item>
 
+        <v-timeline-item
+          color="teal lighten-3"
+          small
+        >
+          <v-layout wrap pt-3>
+            <v-flex xs3>
+              <strong>3-4pm</strong>
+            </v-flex>
+            <v-flex>
+              <strong>Design Stand Up</strong>
+              <div class="caption mb-2">Hangouts</div>
+              <v-avatar>
+                <v-img
+                  src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairFrida&accessoriesType=Kurt&hairColor=Red&facialHairType=BeardLight&facialHairColor=BrownDark&clotheType=GraphicShirt&clotheColor=Gray01&graphicType=Skull&eyeType=Wink&eyebrowType=RaisedExcitedNatural&mouthType=Disbelief&skinColor=Brown"
+                ></v-img>
+              </v-avatar>
+              <v-avatar>
 
+                <v-img
+                  src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairFrizzle&accessoriesType=Prescription02&hairColor=Black&facialHairType=MoustacheMagnum&facialHairColor=BrownDark&clotheType=BlazerSweater&clotheColor=Black&eyeType=Default&eyebrowType=FlatNatural&mouthType=Default&skinColor=Tanned"
+                ></v-img>
+              </v-avatar>
+              <v-avatar>
+                <v-img
+                  src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairMiaWallace&accessoriesType=Sunglasses&hairColor=BlondeGolden&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Surprised&eyebrowType=RaisedExcited&mouthType=Smile&skinColor=Pale"
+                ></v-img>
+              </v-avatar>
+            </v-flex>
+          </v-layout>
+        </v-timeline-item>
 
+        <v-timeline-item
+          color="pink"
+          small
+        >
+          <v-layout pt-3>
+            <v-flex xs3>
+              <strong>12pm</strong>
+            </v-flex>
+            <v-flex>
+              <strong>Lunch break</strong>
+            </v-flex>
+          </v-layout>
+        </v-timeline-item>
+
+        <v-timeline-item
+          color="teal lighten-3"
+          small
+        >
+          <v-layout pt-3>
+            <v-flex xs3>
+              <strong>9-11am</strong>
+            </v-flex>
+            <v-flex>
+              <strong>Finish Home Screen</strong>
+              <div class="caption">Web App</div>
+            </v-flex>
+          </v-layout>
+        </v-timeline-item>
+      </v-timeline>
+    </v-card-text>
+  </v-card>
   </div>
 </template>
 <script>
   export default {
-  /*  data () {
-      return {
-        headers: [
-          {
-            text: 'Dessert (100g serving)',
-            align: 'left',
-            sortable: false,
-            value: 'name'
-          },
-          { text: 'Calories', value: 'calories' },
-          { text: 'Fat (g)', value: 'fat' },
-          { text: 'Carbs (g)', value: 'carbs' },
-          { text: 'Protein (g)', value: 'protein' },
-          { text: 'Iron (%)', value: 'iron' }
-        ],
-        desserts: [
-          {
-            value: false,
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%'
-          },
-          {
-            value: false,
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%'
-          },
-          {
-            value: false,
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%'
-          },
-          {
-            value: false,
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%'
-          },
-          {
-            value: false,
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%'
-          },
-          {
-            value: false,
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%'
-          },
-          {
-            value: false,
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%'
-          },
-          {
-            value: false,
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%'
-          },
-          {
-            value: false,
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%'
-          },
-          {
-            value: false,
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%'
-          }
-        ]
-      }
-    }
-  }
-  */
 
-
-
-
-  data () {
-      return {
-        headers: [
-          {
-            text: 'MOST ACTIVE STOCKS',
-            align: 'left',
-            sortable: false,
-            value: 'name'
-          },
-          { text: 'Price', value: 'price' },
-          { text: 'Volume', value: 'volume' },
-          { text: 'Carbs (g)', value: 'carbs' },
-          { text: 'Protein (g)', value: 'protein' },
-          { text: 'Iron (%)', value: 'iron' }
-        ],
-        desserts: [
-          {
-            value: false,
-            name: 'Advanced Micro Devices Inc.',
-            price: 19.27,
-            volume: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%'
-          },
-          {
-            value: false,
-            name: 'Ford Motor Company',
-            price: 8.99,
-            volume: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%'
-          },
-          {
-            value: false,
-            name: 'General Electric Company',
-            price: 11.8,
-            volume: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%'
-          },
-          {
-            value: false,
-            name: 'Twitter Inc.',
-            price: 31.8,
-            volume: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%'
-          },
-          {
-            value: false,
-            name: 'AT&T Inc.',
-            price: 29.98,
-            volume: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%'
-          },
-          {
-            value: false,
-            name: 'Ambev S.A.',
-            price: 4.04,
-            volume: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%'
-          },
-          {
-            value: false,
-            name: 'Bank of America Corporation',
-            price: 26.59,
-            volume: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%'
-          },
-          {
-            value: false,
-            name: 'Microsoft Corporation',
-            price: 108.3,
-            volume: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%'
-          },
-          {
-            value: false,
-            name: 'Goldcorp Inc.',
-            price: 8.49,
-            volume: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%'
-          },
-          {
-            value: false,
-            name: 'Intel Corporation',
-            price: 44.31,
-            volume: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%'
-          }
-        ]
-      }
-    }
-  }
+}
 </script>
 
